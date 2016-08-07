f3e32fe116cbde03e2177ff1e4bf62dc2cd31666
Vineet Verma <VineetVerma.it@gmail.com>
2016-08-07 12:38:33 +0530

    update for running on heroku

---
+++ src/launch/FrnkServlet.java
@@ -0,0 +1,24 @@
+package launch;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "fr_nkServlet", urlPatterns = { "/hi" })
+public class FrnkServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		ServletOutputStream out = resp.getOutputStream();
+		out.write("hello heroku".getBytes());
+		out.flush();
+		out.close();
+	}
+}
